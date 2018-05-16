4cb23c210b9d0da5b03fd38cdf99cb66e1cc9fdc
dmk99 <jenrel62@gmail.com>
2018-05-16 08:53:08 +0800

    Update solc.js
    Add the `metadata` option to the `combined-json` flags

--- src/compile/solc.js
+++ src/compile/solc.js
@@ -7,7 +7,7 @@ import compile from 'truffle-compile';
 export default function (src) {
   // detect if we're in a truffle project
   return new Promise((resolve) => {
-      const exec = `solc --combined-json abi,asm,ast,bin,bin-runtime,clone-bin,devdoc,interface,opcodes,srcmap,srcmap-runtime,userdoc ${src}`;
+      const exec = `solc --combined-json abi,asm,ast,bin,bin-runtime,clone-bin,devdoc,interface,metadata,opcodes,srcmap,srcmap-runtime,userdoc ${src}`;
       const res = JSON.parse(childProcess.execSync(exec));
       resolve({
         contracts: Object.keys(res.contracts).reduce((o, k) => {
