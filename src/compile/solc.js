43aa4c3dcf887f58616c3f9485a26eb9970f8e70
dmk99 <jenrel62@gmail.com>
2018-05-16 08:52:19 +0800

    Update solc.js
    Remove the truffle check and instead compile the solidity contracts directly. `truffle compile` does not output the metadata by default.

--- src/compile/solc.js
+++ src/compile/solc.js
@@ -7,31 +7,6 @@ import compile from 'truffle-compile';
 export default function (src) {
   // detect if we're in a truffle project
   return new Promise((resolve) => {
-    if (fs.existsSync(`${process.env.PWD}/truffle.js`)) {
-      const config = Config.default();
-      config.resolver = new Resolver(config);
-      config.rawData = true;
-      compile.all(config, (err, res) => {
-        if (err) { throw err; }
-        resolve({
-          contracts: Object.keys(res).reduce((o, k) => {
-            const { metadata, ...data } = res[k].rawData;
-            try {
-              const parsed = JSON.parse(metadata);
-              const fN = Object.keys(parsed.settings.compilationTarget)[0];
-              data.fileName = fN.indexOf(process.env.PWD) === 0 ? fN : `${process.env.PWD}/node_modules/${fN}`;
-              data.output = parsed.output;
-            } catch (e) {
-              console.log(`⚠️ Error parsing Contract: ${k}`);
-            }
-            return {
-              ...o,
-              [k]: data,
-            };
-          }, {}),
-        });
-      });
-    } else {
       const exec = `solc --combined-json abi,asm,ast,bin,bin-runtime,clone-bin,devdoc,interface,opcodes,srcmap,srcmap-runtime,userdoc ${src}`;
       const res = JSON.parse(childProcess.execSync(exec));
       resolve({
@@ -48,11 +23,10 @@ export default function (src) {
               fileName,
               abi: JSON.parse(contract.abi),
               devdoc: JSON.parse(contract.devdoc),
-              userdoc: JSON.parse(contract.userdoc),
+              userdoc: JSON.parse(contract.userdoc),a
             },
           };
         }, {}),
       });
-    }
-  });
+    });
 }
